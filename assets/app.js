4ffa1e41726add75dd45e50441ca3a8376f1637f
yhailoul <yasmine.hailoul2025@campus-eni.fr>
2026-03-09 16:20:33 +0100

    (twig:asset et path)

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
 
+import './styles/normalize.css'
+import './styles/app.css';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
