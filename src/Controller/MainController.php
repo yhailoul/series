d1b34869e1045d8a5826423bccc88e0f933bbce1
yhailoul <yasmine.hailoul2025@campus-eni.fr>
2026-03-09 14:25:30 +0100

    tests with twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,15 +11,17 @@ final class MainController extends AbstractController
     #[Route('', name: 'main_home')]
     public function home(): Response
     {
-        dd("coucou");
-        return $this->render('main/index.html.twig', [
+
+        return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
     #[Route('/test', name: 'main_test')]
     public function test(): Response
     {
-        dd("hello world");
+        return $this->render('main/test.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
 
     }
 }
